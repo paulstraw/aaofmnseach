a47fc7216627d59b964d2f5b11526b044504ecfd
Sean Washington <hello@seanwash.com>
2014-03-05 07:58:24 -0800

    Convert things to grace note.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
+source 'https://rubygems.org'
+
 gem 'sinatra'
+gem 'sinatra-initializers'
+gem 'sinatra-contrib'
+gem 'gracenote', git: 'https://github.com/MattNewberry/gracenote'
 gem 'shotgun'
-gem 'echowrap'
-gem 'discogs-wrapper'
--- app.rb
+++ app.rb
@@ -1,13 +1,29 @@
+require 'rubygems'
 require 'sinatra'
-require 'echowrap'
-require 'discogs-wrapper'
+require 'sinatra/json'
+require 'gracenote'
 
-Echowrap.configure do |config|
-	config.api_key = '9K9E1LQCAUJRHYJ2H'
-	config.consumer_key = '31874c98849a762743b3157440bb351d'
-	config.shared_secret = 'OyPDbXcgQpSwzy6aDCEwlg'
+puts 'Gracenote Begin ============================='
+spec = {clientID: '15780864-DF67FF664D8508CB6FBFC0849D8F3211', clientTag: 'DF67FF664D8508CB6FBFC0849D8F3211'}
+obj = Gracenote.new(spec)
+obj.registerUser
+puts 'Gracenote End ============================='
+
+get '/debug' do
+  results = obj.findArtist('Thrice')
+
+  results.inspect
+
+  # results_type = results.kind_of? Hash
+  # results_type.inspect
 end
 
 get '/' do
-	erb :index
+  erb :index
+end
+
+post '/search' do
+  results = obj.findArtist(params[:name])
+
+  erb :search, locals: {results: results}
 end
