b8fa5a5c9048ddfb404ccb4fc211ab1bbd234236
Sean Washington <hello@seanwash.com>
2014-03-05 20:24:49 -0800

    Commit.

--- app.rb
+++ app.rb
@@ -1,29 +1,19 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/json'
-require 'gracenote'
+require 'nokogiri'
+require 'discogs'
 
-puts 'Gracenote Begin ============================='
-spec = {clientID: '15780864-DF67FF664D8508CB6FBFC0849D8F3211', clientTag: 'DF67FF664D8508CB6FBFC0849D8F3211'}
-obj = Gracenote.new(spec)
-obj.registerUser
-puts 'Gracenote End ============================='
-
-get '/debug' do
-  results = obj.findArtist('Thrice')
-
-  results.inspect
-
-  # results_type = results.kind_of? Hash
-  # results_type.inspect
-end
+wrapper = Discogs::Wrapper.new('bgQQHVZNKAtrsVADRkNK')
 
 get '/' do
   erb :index
 end
 
 post '/search' do
-  results = obj.findArtist(params[:name])
 
-  erb :search, locals: {results: results}
+  response = wrapper.search(params[:name])
+  # results = Nokogiri::XML(response)
+
+  erb :search, locals: {results: response}
 end
